3cee42714c11361ff7f6e5ef75fd23aacae5ebd8
Thomas Trebat <thomastrebat@my.unt.edu>
2024-04-06 19:49:08 -0500

    added activation layer to feature vectors

--- spectral_conv.py
+++ spectral_conv.py
@@ -1,7 +1,12 @@
 import torch
+import torch.nn.functional as F
+from torch_geometric import seed_everything
 from torch_geometric.datasets import KarateClub
 from torch_geometric.utils import get_laplacian
 from scipy.sparse.linalg import eigsh
+
+
+seed_everything(42)
 
 dataset = KarateClub()
 
@@ -14,18 +19,25 @@ data = dataset[0]
 
 print(data)
 
-print(f'Is Undirected?: {data.is_undirected()}')
-print(f'Nodes to Train: {data.train_mask.sum().item()}')
-
 edge_index, edge_weight = get_laplacian(data.edge_index, 
                                         normalization='sym')
 
-laplacian = torch.zeros(size=(data.num_nodes, data.num_nodes))
+n = data.num_nodes
+laplacian = torch.zeros(size=(n, n))
 laplacian[edge_index[0, :], edge_index[1, :]] = edge_weight
 
-print(laplacian)
-print(torch.diag(laplacian))
+K = 3
+eigenvalues, eigenvectors = eigsh(laplacian.numpy(), k=K, which='LM')
+eigenvectors = torch.from_numpy(eigenvectors)
+out = eigenvectors.T @ data.x
 
-eigenvalues, eigenvectors = eigsh(laplacian.numpy(), k=3, which='LM')
+out_channels = 9
+spec = torch.nn.Parameter(torch.randn(out_channels, K, n))
+out = spec * out
+out = out.sum(dim=-1, keepdim=True)
+out = eigenvectors @ out
+out = out.squeeze(-1)
+out = F.softmax(out, dim=-1).T
 
-print(eigenvectors.shape)
+print(out)
+print(out.shape)
